4c46cbe6562151049a15997e9b674626243bab4b
Rowin206 <rowin.amsterdam@gmail.com>
2019-03-15 20:52:35 +0100

    add and configure eslint

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,33 @@
-const express = require('express');
-var router = express.Router(); 
+const express = require("express");
+var router = express.Router();
 
 
-router.get('/', get)
-router.post('/', add)
-router.put('/', update)
-router.delete('/', remove)
+router.get("/", get);
+router.post("/", add);
+router.put("/", update);
+router.delete("/", remove);
 
 
 function get(req, res) {
-    res.render('index.ejs')
+    res.render("index.ejs");
 }
 
 function add(req, res) {
     res.send({
-        type: 'POST'
+        type: "POST"
     });
 }
 
 function update(req, res) {
     res.send({
-        type: 'PUT'
-    })
+        type: "PUT"
+    });
 }
 
 function remove(req, res) {
     res.send({
-        type: 'DELETE'
-    })
+        type: "DELETE"
+    });
 }
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,58 +1,62 @@
-const express = require('express');
+const express = require("express");
 var router = express.Router();
-var find = require('array-find');
-const slug = require('slug');
-const bodyParser = require('body-parser');
-var multer = require('multer')
+var find = require("array-find");
+const slug = require("slug");
+const bodyParser = require("body-parser");
+var multer = require("multer");
 
-var upload = multer({dest: 'static/upload/'})
+var upload = multer({
+    dest: "static/upload/"
+});
 var data = [{
-        id: 'hailey-felton',
-        name: 'Hailey Felton',
-        age: '20',
-        description: 'Hallo dit is Hailey'
-    },
-    {
-        id: 'johan-marton',
-        name: 'John Marton',
-        age: '22',
-        description: 'Hallo dit is John'
-    }
-]
+    id: "hailey-felton",
+    name: "Hailey Felton",
+    age: "20",
+    description: "Hallo dit is Hailey"
+},
+{
+    id: "johan-marton",
+    name: "John Marton",
+    age: "22",
+    description: "Hallo dit is John"
+}
+];
 
-router.use(bodyParser.urlencoded({extended: true}))
+router.use(bodyParser.urlencoded({
+    extended: true
+}));
 router.use(bodyParser.json());
-router.get('/users', get)
-router.post('/users', upload.single('cover'), add)
-router.put('/users', update)
-router.delete('/:id', remove)
-router.get('/add', form)
-router.get('/:id', user)
+router.get("/users", get);
+router.post("/users", upload.single("cover"), add);
+router.put("/users", update);
+router.delete("/:id", remove);
+router.get("/add", form);
+router.get("/:id", user);
 
 function user(req, res, next) {
-    var id = req.params.id
+    var id = req.params.id;
     var user = find(data, function (value) {
-        return value.id === id
-    })
+        return value.id === id;
+    });
 
     if (!user) {
-        next()
-        return
+        next();
+        return;
     }
-    res.render('detail.ejs', {
+    res.render("detail.ejs", {
         data: user
-    })
+    });
 }
 
 
 function get(req, res) {
-    res.render('allUsers.ejs', {
+    res.render("allUsers.ejs", {
         data: data
-    })
+    });
 }
 
 function add(req, res) {
-    var id = slug(req.body.name).toLowerCase()
+    var id = slug(req.body.name).toLowerCase();
 
     data.push({
         id: id,
@@ -60,30 +64,32 @@ function add(req, res) {
         name: req.body.name,
         age: req.body.age,
         description: req.body.description
-    })
+    });
 
-    res.redirect('/' + id)
+    res.redirect("/" + id);
 }
 
 function update(req, res) {
     res.send({
-        type: 'PUT'
-    })
+        type: "PUT"
+    });
 }
 
 function remove(req, res) {
-    var id = req.params.id
+    var id = req.params.id;
 
-  data = data.filter(function (value) {
-    return value.id !== id
-  })
+    data = data.filter(function (value) {
+        return value.id !== id;
+    });
 
-  res.json({status: 'ok'})
+    res.json({
+        status: "ok"
+    });
 
 }
 
 function form(req, res) {
-    res.render('add.ejs')
+    res.render("add.ejs");
 }
 
 

e1543d7128d8d33e6cb9d567823eda16e3680bf0
Rowin206 <rowin.amsterdam@gmail.com>
2019-03-18 18:40:08 +0100

    tried to add mongodb

--- routes/users.js
+++ routes/users.js
@@ -1,71 +1,125 @@
 const express = require("express");
 var router = express.Router();
-var find = require("array-find");
-const slug = require("slug");
+// var find = require("array-find");
+// const slug = require("slug");
 const bodyParser = require("body-parser");
 var multer = require("multer");
+var mongo = require("mongodb");
+
+require("dotenv").config();
+
+var db = null;
+var url = "mongodb://" + process.env.DB_HOST + ":" + process.env.DB_PORT;
+
+mongo.MongoClient.connect(url, {useNewUrlParser: true }, function (err, client) {
+    if (err) throw err;
+    db = client.db(process.env.DB_NAME);
+});
 
 var upload = multer({
     dest: "static/upload/"
 });
-var data = [{
-    id: "hailey-felton",
-    name: "Hailey Felton",
-    age: "20",
-    description: "Hallo dit is Hailey"
-},
-{
-    id: "johan-marton",
-    name: "John Marton",
-    age: "22",
-    description: "Hallo dit is John"
-}
-];
+
+// var data = [{
+//     id: "hailey-felton",
+//     name: "Hailey Felton",
+//     age: "20",
+//     description: "Hallo dit is Hailey"
+// },
+// {
+//     id: "johan-marton",
+//     name: "John Marton",
+//     age: "22",
+//     description: "Hallo dit is John"
+// }
+// ];
 
 router
     .use(bodyParser.urlencoded({extended: true}))
     .use(bodyParser.json())
+
     .get("/users", get)
     .post("/users", upload.single("cover"), add)
     .put("/users", update)
-    .delete("/:id", remove)
     .get("/add", form)
-    .get("/:id", user);
+    .get("/:id", user)
+    .delete("/:id", remove);
+    
 
 function user(req, res, next) {
     var id = req.params.id;
-    var user = find(data, function (value) {
-        return value.id === id;
-    });
-
-    if (!user) {
-        next();
-        return;
+    db.collection("movie").findOne({
+        _id: mongo.ObjectID(id)
+    }, done);
+    
+    function done(err, data) {
+        if (err) {
+            next(err);
+        } else {
+            res.render("detail.ejs", {data: data});
+        }
     }
-    res.render("detail.ejs", {
-        data: user
-    });
+    
+    // var user = find(data, function (value) {
+    //     return value.id === id;
+    // });
+
+    // if (!user) {
+    //     next();
+    //     return;
+    // }
+    // res.render("detail.ejs", {
+    //     data: user
+    // });
 }
 
 
-function get(req, res) {
-    res.render("allUsers.ejs", {
-        data: data
-    });
+// function get(req, res) {
+//     res.render("allUsers.ejs", {
+//         data: data
+//     });
+// }
+
+function get(req, res, next) {
+    db.collection("movie").find().toArray(done);
+  
+    function done(err, data) {
+        if (err) {
+            next(err);
+        } else {
+            res.render("allUsers.ejs", {data: data});
+        }
+    }
 }
+  
 
-function add(req, res) {
-    var id = slug(req.body.name).toLowerCase();
-
-    data.push({
-        id: id,
+function add(req, res, next) {
+    db.collection("movie").insertOne({
+        title: req.body.title,
         cover: req.file ? req.file.filename : null,
-        name: req.body.name,
-        age: req.body.age,
+        plot: req.body.plot,
         description: req.body.description
-    });
+    }, done);
+    
+    function done(err, data) {
+        if (err) {
+            next(err);
+        } else {
+            res.redirect("/" + data.insertedId);
+        }
+    }
+    
+    // var id = slug(req.body.name).toLowerCase();
 
-    res.redirect("/" + id);
+    // data.push({
+    //     id: id,
+    //     cover: req.file ? req.file.filename : null,
+    //     name: req.body.name,
+    //     age: req.body.age,
+    //     description: req.body.description
+    // });
+
+    // res.redirect("/" + id);
 }
 
 function update(req, res) {
@@ -74,16 +128,28 @@ function update(req, res) {
     });
 }
 
-function remove(req, res) {
+function remove(req, res, next) {
     var id = req.params.id;
+    db.collection("movie").deleteOne({
+        _id: mongo.ObjectID(id)
+    }, done);
+    
+    function done(err) {
+        if (err) {
+            next(err);
+        } else {
+            res.json({status: "ok"});
+        }
+    }
+    
 
-    data = data.filter(function (value) {
-        return value.id !== id;
-    });
+    // data = data.filter(function (value) {
+    //     return value.id !== id;
+    // });
 
-    res.json({
-        status: "ok"
-    });
+    // res.json({
+    //     status: "ok"
+    // });
 
 }
 
